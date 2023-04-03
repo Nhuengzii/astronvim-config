7a44c8e67c9736d38f1b441e5c563e225c8ea6ff
Nhuengzii <32nhueng@gmail.com>
2023-04-03 23:34:14 +0700

    Setup discord presence

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,5 +11,14 @@ return {
   -- },
   {
     "andweeb/presence.nvim",
+    lazy = false,
+    config = function()
+      require("presence").setup {
+        autoupdate = true,
+        main_image = "file",
+        neovim_image_text = "Neovim > Other Editor!",
+        editing_text = "ทำอะไรอยู่ก็ไม่รู้ใน  %s",
+      }
+    end,
   },
 }
