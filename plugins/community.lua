c02cd2e0ae8372526a19b92ce6d41530176ddccd
Nhuengzii <32nhueng@gmail.com>
2023-04-05 13:03:29 +0700

    setup copilot.lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,16 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  {
+    "copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+    },
+  },
 }
