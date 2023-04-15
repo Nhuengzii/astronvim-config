cd1a147a114e72d0b70732529b575d11f32a3825
Nhuengzii <32nhueng@gmail.com>
2023-04-15 14:15:47 +0700

    add volar lsp for vue

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "tsserver", "clangd", "gopls", "bashls", "pyright" },
+      ensure_installed = { "lua_ls", "tsserver", "clangd", "gopls", "bashls", "pyright", "volar" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
