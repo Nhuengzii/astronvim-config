35e4bca51001543a5112615484b3e825bdbc183e
Nhuengzii <32nhueng@gmail.com>
2023-08-29 13:47:16 +0700

    change background_colour error

--- init.lua
+++ init.lua
@@ -86,6 +86,9 @@ return {
     --   pattern = {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
+    require("notify").setup({
+      background_colour = "#000000",
+    })
     -- }
   end,
 }
